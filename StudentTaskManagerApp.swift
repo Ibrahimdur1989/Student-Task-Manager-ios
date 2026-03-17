c64e91b79b6f6f47ea8df3dafc29546e98fab912
Ebrahim Al-Serri <ebrahim.al-serri@georgebrown.ca>
2026-03-17 18:37:06 -0400

    Initial base setup for student Task Manager project

---
+++ StudentTaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentTaskManagerApp.swift
+//  StudentTaskManager
+//
+//  Created by Ebrahim Al-Serri on 2026-03-17.
+//
+
+import SwiftUI
+
+@main
+struct StudentTaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
